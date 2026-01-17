1c711a5fa1f01bbc8a590dbf620aa47bb72bf26f
Laki <lakicalinuur@icloud.com>
2026-01-17 13:11:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ YDL_OPTS_PIN = {
 }
 
 YDL_OPTS_YOUTUBE = {
-    "format": "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]",
+    "format": "best",
     "outtmpl": os.path.join(DOWNLOAD_PATH, "%(title)s.%(ext)s"),
     "noplaylist": True,
     "quiet": True,
